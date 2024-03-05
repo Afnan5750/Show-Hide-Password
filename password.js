65cb12f7f915eae1e7b70f88b6ad87e82cea4e85
M Afnan Khadim <afnan.khadim630@gmail.com>
2024-03-05 09:34:02 +0500

    Initial commit

---
+++ password.js
@@ -0,0 +1,14 @@
+let eyeicon = document.getElementById("eye-icon");
+let password = document.getElementById("password");
+
+
+
+eyeicon.onclick = function () {
+    if (password.type === "password") {
+        password.type = "text";
+        eyeicon.src= ("images/eye-open.png");
+    } else {
+        password.type = "password";
+        eyeicon.src= ("images/eye-close.png");
+    }
+}
